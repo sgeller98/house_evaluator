8106880218b0bea4e74e5b477e23cb4b5dda1cea
SammyG <geller.sammy@gmail.com>
2024-11-25 01:17:43 -0500

    Create T3 App initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["house_evaluator_*"],
+} satisfies Config;
